7440ad4ce8fb540d63493fc7d367db38b7f379c1
Stephen Mburu Waithira <stephen.waithira@griffinglobaltech.com>
2024-07-23 15:49:51 +0300

    updated player 2 moves

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ document.querySelectorAll("img") [1].setAttribute("src", randomImageSource2);
 if(randomNumber1 > randomNumber2) {
  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!! 🎉";
 }
-else if (randomNumber2 < randomNumber1) {
- document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!! 🎉";
+else if (randomNumber2 > randomNumber1) {
+ document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!! 🎉";
 }
 else {
 document.querySelector("h1").innerHTML = "Draw!";
